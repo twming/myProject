c4680e68cb6c912cea079e542eda4deffac4fb27
twming <tanwoeiming@gmail.com>
2025-06-06 17:19:28 +0800

    check in substract.py

---
+++ substract.py
@@ -0,0 +1,12 @@
+def add_numbers():
+    # Take two inputs from the user
+    num1 = 1
+    num2 = 2
+    # Subtract the numbers
+    result = num1 - num2
+    # Print the result
+    print(f"The result of subtraction is: {result}")
+
+# Call the function
+if __name__ == "__main__":
+    subtract_numbers()
